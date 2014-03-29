feb81fbd6802ef121f386ef26a0fb48fe2c6988f
Henry Van Styn <vanstyn@intellitree.com>
2014-03-29 11:16:33 -0400

    Refactored resolve_key_path() as recursive algorithm

--- lib/Hash/Layout.pm
+++ lib/Hash/Layout.pm
@@ -18,6 +18,7 @@ has 'levels', is => 'ro', isa => ArrayRef[
 
 sub num_levels { scalar(@{(shift)->levels}) }
 
+has 'default_key',       is => 'ro', isa => Str, default => sub { '*' };
 has 'default_value',     is => 'ro', default => sub { 1 };
 has 'allow_deep_values', is => 'ro', isa => Bool, default => sub { 1 };
 has 'deep_delimiter',    is => 'ro', isa => Str, default => sub { '.' };
@@ -62,12 +63,47 @@ sub coerce {
 }
 
 
+sub _load {
+  my ($self, $index, $noderef, @args) = @_;
+  
+  my $Lvl = $self->levels->[$index] or die "Bad level index '$index'";
+  
+  for my $arg (@args) {
+    if(ref $arg) {
+      if(ref($arg) eq 'HASH') {
+        for my $key (keys %$arg) {
+          my $val = $arg->{$key};
+        
+        
+          # In progress ....
+        
+        
+        }
+      
+      }
+      else {
+        die "Cannot load non-hash";
+      }
+    }
+    else {
+      # hanging string/scalar, recall with default value
+      $self->_load($index,$noderef,{ $arg => $self->default_value });
+    }
+  }
+}
+
+
 sub set {
   my ($self,$key,$value) = @_;
   die "bad number of arguments passed to set" unless (scalar(@_) == 3);
   die '$key value is required' unless ($key && $key ne '');
-
+  
+  my $eval_path = $self->_eval_key_path($key) 
+    or die "Error resolving key string '$key'";
+  
+  eval join('','$self->_Hash->',$eval_path,' = $value');
 }
+
 
 sub _eval_key_path {
   my ($self, $key) = @_;
@@ -76,28 +112,41 @@ sub _eval_key_path {
   return join('',map { '{"'.$_.'"}' } @path);
 }
 
-sub resolve_key_path {
-  my ($self, $key) = @_;
-  
-  my @path = ();
-  for my $Lvl (@{$self->levels}) {
-    my ($k,$leftover) = $Lvl->_peel_key($key);
-    $k = '*' if ($leftover && (!$k || $k eq ''));
-    last unless ($k && $k ne '');
-    push @path, $k;
-    last unless ($leftover && $leftover ne '');
-    $key = $leftover;
-  }
-  
-  if($self->allow_deep_values) {
-    my $last = pop @path;
-    my $del = $self->deep_delimiter;
-    push @path, split(/\Q${del}\E/,$last);
-  }
-  
-  return @path;
-}
 
+sub resolve_key_path {
+  my ($self, $key, $index) = @_;
+  $index ||= 0;
+  
+  my $Lvl = $self->levels->[$index];
+  my $last_level = ! $self->levels->[$index+1];
+  
+  if ($Lvl) {
+    my ($peeled,$leftover) = $Lvl->_peel_str_key($key);
+    if($peeled) {
+      # If a key was peeled, move on to the next level with leftovers:
+      return ($peeled, $self->resolve_key_path($leftover,$index+1)) if ($leftover); 
+      
+      # If there were no leftovers, recurse again only for the last level,
+      # otherwise, return now (this only makes a difference for deep values)
+      return $last_level ? $self->resolve_key_path($peeled,$index+1) : $peeled;
+    }
+    else {
+      # If a key was not peeled, add the default key at the top of the path
+      # only if we're not already at the last level: 
+      my @path = $self->resolve_key_path($key,$index+1);
+      return $last_level ? @path : ($self->default_key,@path);
+    }
+  }
+  else {
+    if($self->allow_deep_values) {
+      my $del = $self->deep_delimiter;
+      return split(/\Q${del}\E/,$key);
+    }
+    else {
+      return $key;
+    }
+  }
+}
 
 
 sub _coerce_levels_param {
--- lib/Hash/Layout/Level.pm
+++ lib/Hash/Layout/Level.pm
@@ -7,14 +7,35 @@ use Types::Standard qw(:all);
 
 has 'name',      is => 'ro', isa => Str, required => 1;
 has 'index',     is => 'ro', isa => Int, required => 1;
-has 'delimiter', is => 'ro', isa => Maybe[Str], default => sub { undef };
+has 'delimiter', is => 'ro', isa => Maybe[Str], default => sub {undef};
 
-sub _peel_key {
+has 'edge_keys', is => 'ro', isa => Maybe[
+  Map[Str,Bool]
+], coerce => \&_coerce_list_hash, default => sub {undef};
+
+has 'deep_keys', is => 'ro', isa => Maybe[
+  Map[Str,Bool]
+], coerce => \&_coerce_list_hash, default => sub {undef};
+
+has 'limit_keys', is => 'ro', isa => Bool, default => sub { 0 };
+
+
+
+# Peel off the prefix key from a concatenated key string, according
+# to this Level's delimiter:
+sub _peel_str_key {
   my ($self,$key) = @_;
-  my $del = $self->delimiter or return $key;
-  return ($key =~ /\Q${del}\E/) ? split(/\Q${del}\E/,$key,2) : ('*',$key);
+  my $del = $self->delimiter or return undef;
+  return undef unless ($key =~ /\Q${del}\E/);
+  my ($peeled,$leftover) = split(/\Q${del}\E/,$key,2);
+  return undef unless ($peeled && $peeled ne '');
+  return ($leftover && $leftover ne '' && wantarray) ? 
+    ($peeled,$leftover) : $peeled;
 }
 
+sub _coerce_list_hash {
+  $_[0] && ! ref($_[0]) ? { $_[0] => 1 } :
+  ref($_[0]) eq 'ARRAY' ? { map {$_=>1} @{$_[0]} } : $_[0];
+}
 
 1;
-
