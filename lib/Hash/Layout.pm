36629362e4cd110eda95b622ab6d0a5a5c8eaca2
Henry Van Styn <vanstyn@intellitree.com>
2020-11-21 09:36:47 -0500

    v2.00
    Updated travis cfg

--- lib/Hash/Layout.pm
+++ lib/Hash/Layout.pm
@@ -4,7 +4,7 @@ use warnings;
 
 # ABSTRACT: hashes with predefined layouts, composite keys and default values
 
-our $VERSION = 1.02_04;
+our $VERSION = '2.00';
 
 use Moo;
 use Types::Standard qw(:all);
